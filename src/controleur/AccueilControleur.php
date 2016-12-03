db0fce7b3b775a27832b4cd20d1f0a2492d7dd79
gaetanlussagnet <gaetan.lussagnet@gmail.com>
2016-12-03 16:34:02 +0100

    Acceuil + equipe

--- src/controleur/AccueilControleur.php
+++ src/controleur/AccueilControleur.php
@@ -4,27 +4,16 @@
  * 
  * @author gaetanlussagnet
  */
-class AccueilControleur {
-	
-	/**
-	 * Initialisation de twig
-	 * @return Twig_Environment
-	 */
-	private static function before(){
-		$ini_array = parse_ini_file("configuration.ini");
-		Twig_Autoloader::register();
-		$loader = new Twig_Loader_Filesystem('src/vue');
-		$twig = new Twig_Environment($loader, array('cache' => 'cache','auto_reload'=>$ini_array['autoreload']));
+require_once "src/controleur/Controleur.php";
 		
-		return $twig;
-	}
+class AccueilControleur extends Controleur{
 	
 	/**
 	 * Route accueil/index
 	 * @param array $arg
 	 */
 	public static function index ($arg = array()){
-		$twig = self::before();
+		$twig = parent::before();
 		
 		//Arguments du template
 		$coucou = count($arg) > 0 ? $arg[0] : '';
